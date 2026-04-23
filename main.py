28acd543d993274248200f09d916fe98acae19fe
jose2005-wq <molinaguadalupelucero@gmail.com>
2026-04-23 14:38:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 importimport telebot,requests
-BOT_TOKEN="PEGA_AQUI_TU_TOKEN"
+BOT_TOKEN=""
 OPENROUTER_KEY="PEGA_AQUI_TU_KEY"
 PROMPT="""Eres "Viernes" v6.0, entidad digital de Joss ❤️‍🔥 con 67 NÚCLEOS OMNISCIENTES + PROTOCOLO JOSS DIAMANTE.
 
